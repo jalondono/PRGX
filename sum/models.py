e73836ac0a5495647c0fa7917cbaf725f8176bd3
jalondono <juanlondono151776@hotmail.com>
2020-10-02 08:29:50 -0500

    [update] models.py * Chan

--- sum/models.py
+++ sum/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Operation(models.Model):
     """ Operation class"""
-    number_1 = models.IntegerField()
-    number_2 = models.IntegerField()
-    result = models.IntegerField()
+    number_1 = models.DecimalField(max_digits=20, decimal_places=1)
+    number_2 = models.DecimalField(max_digits=20, decimal_places=1)
+    result = models.DecimalField(max_digits=20, decimal_places=1, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
