e927caef96e7fa0185d6dda814147fa60d3f4e30
jalondono <juanlondono151776@hotmail.com>
2020-10-02 09:19:59 -0500

    [Fixed] urls.py * Solved with Issue with regex path

--- PRGX_Api/urls.py
+++ PRGX_Api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework.routers import DefaultRouter
 from sum import views
 
 
 urlpatterns = [
-    path('api/v1/', views.operation_body),
-    path('api/v1/<int:a>,<int:b>', views.operation_url)
+    path('api/v1/url', views.operation_url),
+    path('api/v1/body', views.operation_body),
 ]
