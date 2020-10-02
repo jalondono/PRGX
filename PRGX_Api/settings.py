f836fbae7b14bca4ed83cfb9e606f1ffa331b8e1
jalondono <juanlondono151776@hotmail.com>
2020-10-02 07:03:19 -0500

    [update] settings.py * Registered rest_framework app and corsheaders

--- PRGX_Api/settings.py
+++ PRGX_Api/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sum'
+    'sum',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
